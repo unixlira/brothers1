e32c4b41d1e94550f35bf113fece2520a94517a6
José Lira <joserobertolira@gmail.com>
2021-07-23 23:22:09 -0300

    Ajustes css index

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -73,21 +73,21 @@
         </div>
     </header>
     <!--Carrousel-->
-    <section >
+    <section class="carrousselIndex">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
             {{--                <ol class="carousel-indicators">--}}
             {{--                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>--}}
             {{--                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>--}}
             {{--                </ol>--}}
-            <div class="carousel-inner" role="listbox" style="height: 700px;">
+            <div id="tamanhoSection"" " class="carousel-inner" role="listbox">
                 <!-- Slide One - Set the background image for this slide in the line below -->
-                <img class="carousel-item active" width="570" src="{{ asset("images/background1.jpg") }}" />
+                <img class="carousel-item active img1" src="{{ asset("images/background1.jpg") }}" />
                 <!-- Slide Two - Set the background image for this slide in the line below -->
                 <img class="carousel-item" src="{{ asset("images/background2.jpg") }}" />
             </div>
         </div>
     </section>
-    <section class="img-responsive" style="background: #ecf1f8 url('{{ asset("images/sectionGrid.png") }}') ;background-repeat: no-repeat;background-size: 100% 100%;height: 1000px;">
+    <section class="sectionSlideCar1" style="background: #ecf1f8 url('{{ asset("images/sectionGrid.png") }}') ;background-repeat: no-repeat;background-size: 100% 100%;">
         <div class="container">
             <div class="sectionSearch ">
                 <div id="tabSearch">
@@ -99,8 +99,8 @@
                     <div class="tab-content clearfix">
                         <form action="{{ route('buscar') }}" method="GET">
                             <div class="tab-pane active" id="1b">
-                                <div class="row">
-                                <div class="col-sm-5">
+                                <div class="row col-12">
+                                <div class="col-sm-12 col-md-4 col-lg-4 mb-1">
                                         <select id="marca" name="marca">
                                             <option value="false" selected="selected">Marca</option>
                                             @foreach($selectMarca as $node)
@@ -108,7 +108,7 @@
                                             @endforeach
                                         </select>
                                     </div>
-                                    <div class="col-sm-5" id="modelo">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-1" id="modelo">
                                         <select name="modelo" >
                                             <option value="false" selected="selected">Modelo</option>
                                             @foreach($selectModelo as $node)
@@ -116,12 +116,12 @@
                                             @endforeach
                                         </select>
                                     </div>
-                                    <div class="col-sm-5" id="select2" style="display: none;">
+                                    <div class="col-sm-12 col-md-4 col-lg-4 mb-1" id="select2" style="display: none;">
                                         <select name ="modelo" id="showModelo">
                                             <option value="false">Modelo</option>
                                         </select>
                                     </div>
-                                    <div class="col-sm">
+                                    <div class="col-sm-12 col-md-4 col-lg-4">
                                         <button type="submit" class="btn btn-danger" id="sent"><i class="novi-icon fa fa-search"></i> Buscar</button>
                                     </div>
                                 </div>
@@ -134,7 +134,7 @@
                 <h1>AS MELHORES NEGOCIAÇÕES ESTÃO AQUI</h1>
                 <hr class="divider divider-lg bg-danger">
             </div>
-            <div class="owl-carousel owl-carousel-classic owl-carousel-class-light veil-owl-nav owl-carousel-testimonials-3 slide" data-items="1" data-md-items="2" data-lg-items="3" data-xl-items="3" data-nav="false" data-dots="true" data-margin="50px" data-autoplay="true" data-autoloop="true">
+            <div class="owl-carousel owl-carousel-classic owl-carousel-class-light veil-owl-nav owl-carousel-testimonials-3 slide" data-items="1" data-md-items="2" data-lg-items="3" data-xl-items="3" data-nav="false" data-dots="true" data-margin="50px" data-autoplay="true" data-loop="true">
                 @foreach($newXml as $node)
                     <div>
                         <blockquote class="quote quote-slider-4 unit uni-spacing-sm flex-md-row">
@@ -164,26 +164,26 @@
             </div>
         </div>
     </section>
-    <section  class="section-98 section-md-110  novi-background" data-preset='{"title":"Blog 1","category":"content, blog","reload":true,"id":"blog-1"}' >
+    <section class="section section-85" >
         <div class="container twoPictures" >
             <div class="row justify-content-sm-center" >
                 <div class="col-md-6 col-lg-6 mb-2">
                     <a class="thumbnail-apollo" href="/compras">
                         <figure><img  src="{{ asset("images/compramos.jpg") }}" alt="Compramos seu veículo"/></figure></a>
                 </div>
-                <div class="col-md-6 col-lg-6  mb-2">
-                    <a class="thumbnail-apollo" href="/analise">   
+                <div class="col-md-6 col-lg-6 ">
+                    <a class="thumbnail-apollo" href="/analise">
                         <figure><img src="{{ asset("images/financia.jpg") }}" alt="Analise de crédito"/></figure></a>
                 </div>
             </div>
         </div>
-        <br>
-        <br>
-        <div class="mt-5 mb-5">
+    </section>
+    <section  class="section-41  novi-background" >
+        <div class="mb-5">
             <h1>MILHARES DE OPÇÕES PARA VOCÊ</h1>
         </div>
         <div class="container">
-            <div class="owl-carousel owl-carousel-classic owl-carousel-class-light veil-owl-nav owl-carousel-testimonials-3 slide mt-5" data-items="1" data-md-items="2" data-lg-items="3" data-xl-items="3" data-nav="false" data-dots="false" data-margin="50px" data-autoplay="true">
+            <div class="owl-carousel owl-carousel-classic owl-carousel-class-light veil-owl-nav owl-carousel-testimonials-3 slide mt-5" data-items="1" data-md-items="2" data-lg-items="3" data-xl-items="3" data-nav="false" data-dots="false" data-margin="50px" data-autoplay="true" data-loop="true">
                 <blockquote class="quote quote-slider-4 unit uni-spacing-sm flex-md-row">
                     <div class="unit-left"></div>
                     <div class="unit-body text-left">
@@ -325,11 +325,11 @@
             </div>
         </div>
     </section>
-    <section class="section novi-background section-66" style="overflow: hidden;background-color: #1a202c">
+    <section class="section  section-66" style="overflow: hidden;background-color: #1a202c">
         <div class="container">
             <div class="row justify-content-sm-center">
                 <div class="col-md-10 col-xl-4">
-                    <div class="unit unit-sm flex-md-row text-md-left ">
+                    <div class="unit unit-sm flex-md-row  ">
                         <div class="unit-body text-center text-white">
                             <h4 class="font-weight-bold text-uppercase offset-md-top-24 text-white">Loja 1</h4>
                             <p>
@@ -341,7 +341,7 @@
                     </div>
                 </div>
                 <div class="col-md-10 col-xl-4 offset-top-50 offset-lg-top-0">
-                    <div class="unit unit-sm flex-md-row text-md-left">
+                    <div class="unit unit-sm flex-md-row ">
                         <div class="unit-body text-center text-white">
                             <h4 class="font-weight-bold text-uppercase offset-md-top-24 text-white">Loja 2</h4>
                             <p>Rua Sete de Setembro, 325 , Centro
@@ -352,7 +352,7 @@
                     </div>
                 </div>
                 <div class="col-md-10 col-xl-4 offset-top-50 offset-lg-top-0">
-                    <div class="unit unit-sm flex-md-row text-md-left">
+                    <div class="unit unit-sm flex-md-row ">
                         <div class="unit-body text-center text-white">
                             <h4 class="font-weight-bold text-uppercase offset-md-top-24 text-white">Loja 3</h4>
                             <p>RAv. General Osório, 1620 , Vl. Trujillo
@@ -366,13 +366,13 @@
         </div>
     </section>
     <hr style="background-color: #1a202c;margin: auto; border-color: #6c757d">
-    <section class="section novi-background section-66" style="overflow: hidden;background-color: #1a202c">
+    <section class="section section-66" style="overflow: hidden;background-color: #1a202c">
         <div class="container">
                 <div class="row justify-content-sm-left">
                     <div class="col-md-10 col-xl-12">
-                        <div class="unit unit-sm flex-md-row text-md-left ">
-                            <div class="unit-body row text-center text-justify offset-md-top-10 text-white">
-                                <p style="color: #718096">© 2021 <a href="/" style="color: #0E7EB5">Smart Veículos</a> | Os Melhores Veículos Semi novos estão na Smart Veículos. Reservamos o direito de eventuais erros de digitação na descrição, valores ou fotos. O valor descrito nos anúncios em nosso site, são para pagamentos à vista, para financiamento e troca, fale com um de nossos vendedores.</p>
+                        <div class="unit unit-sm flex-md-row  ">
+                            <div class="text-center text-white">
+                                <p style="color: #718096">© 2021 <a href="/" style="color: #0E7EB5">Smart Veículos</a> | Os Melhores Veículos Semi novos estão na Smart Veículos. Reservamos o direito de eventuais erros de digitação na descrição, valores ou fotos.&nbsp;O valor descrito nos anúncios em nosso site, são para pagamentos à vista, para financiamento e troca, fale com um de nossos vendedores.</p>
                                 <br>
                             </div>
                         </div>
@@ -380,23 +380,23 @@
                 </div>
                 <div class="container text-center">
                     <div class="redes">
-                        <a href="mailto:vendas.websmartveiculos@gmail.com" class="email mr-5" target="_blank"> <svg id="mail" xmlns="http://www.w3.org/2000/svg" width="40.882" height="31.141" viewBox="0 0 40.882 31.141">
+                        <a href="mailto:vendas.websmartveiculos@gmail.com" class="email mr-3" target="_blank"> <svg id="mail" xmlns="http://www.w3.org/2000/svg" width="40.882" height="31.141" viewBox="0 0 40.882 31.141">
                                 <g id="Grupo_612" data-name="Grupo 612" transform="translate(0 0)">
                                     <path id="Caminho_926" data-name="Caminho 926" d="M37.289,61H3.593A3.6,3.6,0,0,0,0,64.593V88.547a3.6,3.6,0,0,0,3.593,3.593h33.7a3.6,3.6,0,0,0,3.593-3.593V64.593A3.6,3.6,0,0,0,37.289,61Zm-.5,2.4L20.517,79.671,4.1,63.4ZM2.4,88.051V65.078L13.932,76.515Zm1.694,1.694L15.633,78.2l4.045,4.01a1.2,1.2,0,0,0,1.69,0l3.944-3.944L36.793,89.745Zm34.4-1.694L27.005,76.57,38.487,65.089Z" transform="translate(0 -61)" />
                                 </g>
-                            </svg> 
+                            </svg>
                         </a>
-                        <a href="https://api.whatsapp.com/send?phone=5515997121257&text=Ol%C3%A1%2C%20estou%20no%20site%20da%20Smart%20Ve%C3%ADculos%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es." class="whatsapp mr-5" target="_blank"> <svg xmlns="http://www.w3.org/2000/svg" width="30.423" height="30.57" viewBox="0 0 30.423 30.57">
+                        <a href="https://api.whatsapp.com/send?phone=5515997121257&text=Ol%C3%A1%2C%20estou%20no%20site%20da%20Smart%20Ve%C3%ADculos%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es." class="whatsapp mr-3" target="_blank"> <svg xmlns="http://www.w3.org/2000/svg" width="30.423" height="30.57" viewBox="0 0 30.423 30.57">
                                 <path id="whatsapp" d="M26.241,4.443A15.152,15.152,0,0,0,2.4,22.72L.25,30.57l8.03-2.107a15.132,15.132,0,0,0,7.239,1.844h.006A15.155,15.155,0,0,0,26.241,4.443ZM15.525,27.749H15.52a12.575,12.575,0,0,1-6.408-1.755l-.46-.273-4.765,1.25,1.272-4.646-.3-.476a12.59,12.59,0,1,1,10.666,5.9Zm6.906-9.43c-.378-.19-2.239-1.1-2.586-1.231s-.6-.189-.851.19-.978,1.231-1.2,1.484-.441.284-.82.095a10.338,10.338,0,0,1-3.044-1.879,11.417,11.417,0,0,1-2.106-2.622c-.221-.379,0-.564.166-.772a10.707,10.707,0,0,0,.946-1.295.7.7,0,0,0-.032-.663c-.094-.189-.851-2.052-1.167-2.81-.307-.738-.619-.638-.852-.65s-.473-.013-.725-.013a1.39,1.39,0,0,0-1.009.474,4.245,4.245,0,0,0-1.325,3.158A7.363,7.363,0,0,0,9.374,15.7c.189.253,2.669,4.076,6.466,5.715a21.665,21.665,0,0,0,2.158.8,5.189,5.189,0,0,0,2.384.15,3.9,3.9,0,0,0,2.555-1.8,3.163,3.163,0,0,0,.221-1.8c-.094-.158-.347-.252-.725-.442Zm0,0" transform="translate(-0.25)" fill="#006b12" fill-rule="evenodd" />
-                            </svg> 
+                            </svg>
                         </a>
-                        <a href="https://www.facebook.com/SmartVeiculosSorocaba" class="facebook mr-5" target="_blank"> <svg id="facebook" xmlns="http://www.w3.org/2000/svg" width="30.63" height="30.63" viewBox="0 0 30.63 30.63">
+                        <a href="https://www.facebook.com/SmartVeiculosSorocaba" class="facebook mr-3" target="_blank"> <svg id="facebook" xmlns="http://www.w3.org/2000/svg" width="30.63" height="30.63" viewBox="0 0 30.63 30.63">
                                 <g id="Grupo_527" data-name="Grupo 527">
                                     <path id="Caminho_862" data-name="Caminho 862" d="M27.041,0H3.589A3.594,3.594,0,0,0,0,3.589V27.041A3.594,3.594,0,0,0,3.589,30.63H27.041a3.594,3.594,0,0,0,3.589-3.589V3.589A3.594,3.594,0,0,0,27.041,0Zm1.2,27.041a1.2,1.2,0,0,1-1.2,1.2h-6.82V18.486h3.7l.611-3.709H20.221V12.2a1.765,1.765,0,0,1,1.795-1.795h2.453V6.7H22.016a5.5,5.5,0,0,0-5.5,5.514v2.562H12.922v3.709h3.591v9.751H3.589a1.2,1.2,0,0,1-1.2-1.2V3.589a1.2,1.2,0,0,1,1.2-1.2H27.041a1.2,1.2,0,0,1,1.2,1.2Z" fill="#0052cc" />
                                 </g>
-                            </svg> 
+                            </svg>
                         </a>
-                        <a href=" https://www.instagram.com/smartveiculos/?hl=pt-br" class="instagram mr-5" target="_blank"> <svg id="instagram-sketched" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30.57" height="30.569" viewBox="0 0 30.57 30.569">
+                        <a href=" https://www.instagram.com/smartveiculos/?hl=pt-br" class="instagram mr-3" target="_blank"> <svg id="instagram-sketched" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30.57" height="30.569" viewBox="0 0 30.57 30.569">
                                 <defs>
                                     <linearGradient id="linear-gradient" x1="0.084" y1="0.916" x2="0.916" y2="0.084" gradientUnits="objectBoundingBox">
                                         <stop offset="0" stop-color="#ffd600" />
@@ -419,7 +419,7 @@
                             </svg> </a>
                         <a href="https://www.youtube.com/channel/UCgzsmY1p7pG1x-WQemslRSQ" class="youtube" target="_blank"> <svg id="internet" xmlns="http://www.w3.org/2000/svg" width="30.318" height="33.791" viewBox="0 0 30.318 33.791">
                                 <path id="Caminho_865" data-name="Caminho 865" d="M31.288,19.472v9.961a3.71,3.71,0,0,1-.4,1.711,4.426,4.426,0,0,1-1.08,1.377,5.183,5.183,0,0,1-1.575.929,5.456,5.456,0,0,1-1.939.34H5.938a5.385,5.385,0,0,1-1.916-.34,5.149,5.149,0,0,1-1.577-.929,4.567,4.567,0,0,1-1.081-1.377A3.732,3.732,0,0,1,.97,29.433V19.472a3.761,3.761,0,0,1,.4-1.69A4.3,4.3,0,0,1,2.447,16.4a5.5,5.5,0,0,1,1.577-.931,5.237,5.237,0,0,1,1.916-.351H26.3a5.33,5.33,0,0,1,1.926.351,5.484,5.484,0,0,1,1.588.931,4.262,4.262,0,0,1,1.08,1.384A3.759,3.759,0,0,1,31.288,19.472ZM7.3,30.545V20.038H9.8V18.45H3.057v1.543H5.218V30.545ZM8.321,6.1,5.53,0H7.959L9.348,4.562,10.732,0h2.426L10.544,6.1V13.21H8.321Zm6.561,15.478H12.8V27.19c0,.406,0,.721.02.938s-.016.362-.091.421a.788.788,0,0,1-.537.533c-.288.111-.512-.047-.668-.489a1.07,1.07,0,0,1-.02-.44c.014-.236.02-.542.02-.922l-.043-5.65H9.389l.048,5.562c0,.437-.011.8-.025,1.1a2.642,2.642,0,0,0,.025.67c.029.245.055.5.088.784a.876.876,0,0,0,.433.67,1.573,1.573,0,0,0,.847.229,2.883,2.883,0,0,0,.956-.163,3.4,3.4,0,0,0,.861-.439,1.931,1.931,0,0,0,.576-.6l-.041,1.11,1.724.045ZM12.974,7.036A8.012,8.012,0,0,1,13.2,4.973a2.089,2.089,0,0,1,.888-1.207,2.567,2.567,0,0,1,1.566-.455,3.3,3.3,0,0,1,1.407.272,1.964,1.964,0,0,1,.929.795,3.016,3.016,0,0,1,.41.933,7.087,7.087,0,0,1,.134,1.522V9.213a9.354,9.354,0,0,1-.134,1.865,2.671,2.671,0,0,1-.458,1.156,2.018,2.018,0,0,1-.881.748,2.956,2.956,0,0,1-1.3.27,3.864,3.864,0,0,1-1.359-.227,1.4,1.4,0,0,1-.84-.655,3.188,3.188,0,0,1-.458-1.024,9.551,9.551,0,0,1-.136-1.86V7.036Zm1.862,3.7a.911.911,0,0,0,.274.648.882.882,0,0,0,.655.281.912.912,0,0,0,.648-.272.87.87,0,0,0,.281-.657V5.809a.932.932,0,0,0-.281-.689.9.9,0,0,0-.646-.286.882.882,0,0,0-.644.286.937.937,0,0,0-.286.689ZM21.62,27.981V23.329a2.037,2.037,0,0,0-.949-1.9,1.462,1.462,0,0,0-.729-.183,3.08,3.08,0,0,0-1.724.7V18.45H16.131V30.455l1.726-.045.134-.748a3.89,3.89,0,0,0,1.509.863,1.467,1.467,0,0,0,1.135-.127,1.805,1.805,0,0,0,.725-.931A3.994,3.994,0,0,0,21.62,27.981ZM19.105,22.4a1.033,1.033,0,0,1,.644.217.613.613,0,0,1,.283.485v5.267a.586.586,0,0,1-.283.474,1.084,1.084,0,0,1-.644.206,1.1,1.1,0,0,1-.652-.206.581.581,0,0,1-.277-.474V23.1a.613.613,0,0,1,.277-.485A1.05,1.05,0,0,1,19.105,22.4Zm.972-10.12a1.328,1.328,0,0,1-.111-.376,2.82,2.82,0,0,0-.072-.374,3.474,3.474,0,0,1-.043-.616V3.679H21.8v7.215a.656.656,0,0,0,.2.478.755.755,0,0,0,.546.2.79.79,0,0,0,.544-.2.622.622,0,0,0,.233-.478V3.679h1.81v9.259H22.829l.041-.748a3.661,3.661,0,0,1-.612.7,1.473,1.473,0,0,1-.795.227,1.539,1.539,0,0,1-.863-.227A1.727,1.727,0,0,1,20.077,12.279ZM28.2,27.394H26.638v1.019a.676.676,0,0,1-.261.566.967.967,0,0,1-.621.2h-.324a.881.881,0,0,1-.6-.2.705.705,0,0,1-.242-.566V26.256H28.2V25.008c0-.467-.007-.918-.023-1.348a4.982,4.982,0,0,0-.111-1.008,1.485,1.485,0,0,0-.559-.99,2.552,2.552,0,0,0-1.087-.508,3.45,3.45,0,0,0-1.273-.047,2.534,2.534,0,0,0-1.124.43,2.333,2.333,0,0,0-.883,1.115A5.534,5.534,0,0,0,22.87,24.6V27.4a3.058,3.058,0,0,0,.933,2.517,2.829,2.829,0,0,0,1.812.7H25.8a2.809,2.809,0,0,0,1.885-.908A2.18,2.18,0,0,0,28.3,28.1C28.275,27.885,28.25,27.648,28.2,27.394Zm-1.611-2.66h-2l.045-1.108a.775.775,0,0,1,.272-.6.965.965,0,0,1,.661-.24h.091a1.087,1.087,0,0,1,.691.227.757.757,0,0,1,.285.616Z" transform="translate(-0.97)" fill="#e20000" />
-                            </svg> 
+                            </svg>
                         </a>
                     </div>
                 </div>
