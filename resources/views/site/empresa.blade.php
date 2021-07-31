a3545150fa123639c239f2cf3f0c1ab48eb7e16c
Daniella Lira <73200886+Danilira@users.noreply.github.com>
2021-07-30 21:37:26 -0300

    background do input

--- resources/views/site/empresa.blade.php
+++ resources/views/site/empresa.blade.php
@@ -148,7 +148,7 @@
                                 <div class="row">
                                     <div class="col-md-9 mb-2">
                                         <div class="form-group">
-                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" style="background:#ecf1f8" placeholder="Seu email">
 
                                         </div>
                                     </div>
